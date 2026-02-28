045901b3ba75705d791681b201acf8b6ab30b1ab
Amith Vikram <amithvik1@gmail.com>
2026-02-28 14:12:10 +0000

    Sync LeetCode submission - Palindrome Number - Runtime - 12 ms (28.58%), Memory - 19.3 MB (53.61%)

---
+++ solutions/0009-palindrome-number/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        y = str(x)
+        z = y[::-1]
+        if y == z:
+            return True
+        else:
+            return False
+
+
