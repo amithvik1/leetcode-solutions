d111c6ef0e20519da09e9423217a361b941c8981
Amith Vikram <amithvik1@gmail.com>
2026-06-01 18:49:24 +0000

    Sync LeetCode submission - Reverse Integer - Runtime - 0 ms (100.00%), Memory - 9.4 MB (9.00%)

---
+++ solutions/0007-reverse-integer/solution.c
@@ -0,0 +1,13 @@
+int reverse(int x){
+    long rev=0; 
+    int rem; 
+    while(x != 0){
+        rem = x%10; 
+        rev = rev*10 + rem; 
+        x /= 10; 
+    }
+    if (rev > INT_MAX || rev < INT_MIN) {
+        return 0;
+    }
+    return (int)rev;
+}
