75ac1875de390c8c3e3bcc3c07a7054da2e28148
Amith Vikram <amithvik1@gmail.com>
2026-06-07 10:42:30 +0000

    Sync LeetCode submission - Rearrange Array Elements by Sign - Runtime - 8 ms (35.27%), Memory - 104.6 MB (92.00%)

---
+++ solutions/2271-rearrange-array-elements-by-sign/solution.c
@@ -0,0 +1,22 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* rearrangeArray(int* nums, int size, int* returnSize) {
+    *returnSize = size; 
+    int *arr = (int*)malloc(size*sizeof(int)); 
+    int i; 
+    int j =1;
+    int k =0; 
+    for(i = 0; i < size; i++){
+        if(nums[i]<0){ 
+            arr[j] = nums[i]; 
+            j+=2; 
+        }
+        else{
+            arr[k] = nums[i];
+            k+=2; 
+        }
+        
+    }
+    return arr; 
+}
