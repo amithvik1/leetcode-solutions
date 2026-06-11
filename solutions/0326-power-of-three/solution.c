16b1b35919173d66db932947bcddc8d667cc0ed8
Amith Vikram <amithvik1@gmail.com>
2026-06-11 16:44:25 +0000

    Sync LeetCode submission - Power of Three - Runtime - 0 ms (100.00%), Memory - 9.1 MB (94.90%)

---
+++ solutions/0326-power-of-three/solution.c
@@ -0,0 +1,12 @@
+bool isPowerOfThree(int n) {
+    int rem; 
+    if(n<1) return false;
+    while(n>1){
+        rem = n%3; 
+        if(rem != 0){
+            return false; 
+        }
+        n /= 3;
+    }
+    return true;
+}
