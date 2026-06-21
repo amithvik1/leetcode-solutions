9c6a59f7f9cde460bec4dfab54c9b8fdf1cfa386
Amith Vikram <amithvik1@gmail.com>
2026-06-21 09:00:00 +0000

    Sync LeetCode submission - Find Smallest Letter Greater Than Target - Runtime - 0 ms (100.00%), Memory - 9.2 MB (56.21%)

---
+++ solutions/0745-find-smallest-letter-greater-than-target/solution.c
@@ -0,0 +1,14 @@
+char nextGreatestLetter(char* letters, int size, char target) {
+    int left = 0; 
+    int right = size -1;  
+    while (left <= right){
+        int mid = left + (right-left)/2; 
+        if(letters[mid] <= target){
+            left = mid+1; 
+        }
+        else{
+            right = mid -1; 
+        }
+    }
+    return letters[left%size]; 
+}
