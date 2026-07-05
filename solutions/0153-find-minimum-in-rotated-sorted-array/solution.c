276c8bf9896f888612beba95aecf91f17964791f
Amith Vikram <amithvik1@gmail.com>
2026-07-05 13:37:30 +0000

    Sync LeetCode submission - Find Minimum in Rotated Sorted Array - Runtime - 0 ms (100.00%), Memory - 9.2 MB (10.07%)

---
+++ solutions/0153-find-minimum-in-rotated-sorted-array/solution.c
@@ -0,0 +1,14 @@
+int findMin(int* nums, int size) {
+    int left = 0; 
+    int right = size - 1; 
+    while(left < right){
+        int mid = left + (right - left)/2;
+        if (nums[mid] > nums[right]) {
+            left = mid + 1;
+        } 
+        else{
+            right = mid; 
+        }
+    }
+    return nums[left]; 
+}
