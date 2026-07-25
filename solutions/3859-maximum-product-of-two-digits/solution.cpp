d4cda58bb652e976c35148b51b899c8e3485ba30
Amith Vikram <amithvik1@gmail.com>
2026-07-25 04:21:50 +0000

    Sync LeetCode submission - Maximum Product of Two Digits - Runtime - 3 ms (14.82%), Memory - 8.8 MB (59.04%)

---
+++ solutions/3859-maximum-product-of-two-digits/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProduct(int n) {
+        int a = 0; 
+        int b = 0; 
+        while(n > 0){
+            int rem = n % 10; 
+            if(rem > a){
+                b = a; 
+                a = rem; 
+            }
+            else if(rem > b){
+                b = rem; 
+            }
+            n /= 10; 
+        }
+        return a*b; 
+    }
+};
