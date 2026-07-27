4701707d94aaef7e565fca3d13d9355a93ce1023
Amith Vikram <amithvik1@gmail.com>
2026-07-27 09:33:22 +0000

    Sync LeetCode submission - Kth Missing Positive Number - Runtime - 0 ms (100.00%), Memory - 13.3 MB (29.62%)

---
+++ solutions/1646-kth-missing-positive-number/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int current = 1; 
+        int i = 0; 
+        while(k > 0){
+            if(i < arr.size() && arr[i] == current){
+                i++; 
+            }
+            else k--; 
+            if(k == 0) return current; 
+            current++; 
+        }
+        return current; 
+    }
+};
