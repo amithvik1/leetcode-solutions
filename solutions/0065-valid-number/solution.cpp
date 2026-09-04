6483d5dcabf89f7297802ce621766ad2583aa5f5
Amith Vikram <amithvik1@gmail.com>
2026-09-04 15:15:28 +0000

    Sync LeetCode submission - Valid Number - Runtime - 0 ms (100.00%), Memory - 8.1 MB (59.86%)

---
+++ solutions/0065-valid-number/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isNumber(string s) {
+        bool digit = false; 
+        bool exp = false; 
+        bool dot = false; 
+
+        for(int i = 0; i < s.length(); i++){
+            char c = s[i]; 
+            if(isdigit(c)) digit = true; 
+            else if(c == '+' || c == '-'){
+                if(i > 0 && s[i-1] != 'e' && s[i-1] != 'E'){
+                    return false; 
+                }
+            }
+            else if(c == '.'){
+                if(exp || dot) return false; 
+                dot = true; 
+            }
+            else if(c == 'e' || c == 'E'){
+                if(exp || !digit) return false; 
+                exp = true; 
+                digit = false; 
+            }
+            else return false; 
+        }
+        return digit; 
+    }
+};
