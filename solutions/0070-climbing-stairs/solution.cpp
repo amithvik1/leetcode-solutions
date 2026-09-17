d9e1b3554b4a30b291a0451ddc6bcaf4d241d621
Amith Vikram <amithvik1@gmail.com>
2026-09-17 12:51:08 +0000

    Sync LeetCode submission - Climbing Stairs - Runtime - 0 ms (100.00%), Memory - 7.8 MB (73.57%)

---
+++ solutions/0070-climbing-stairs/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+    int i; 
+    int a=1,b=2,next; 
+    if (n == 1) return a;
+    if (n == 2) return b;
+    for(i=3;i<=n;i++){
+        next = a + b;
+        a = b;
+        b = next;
+    }
+    return b;
+    }
+};
